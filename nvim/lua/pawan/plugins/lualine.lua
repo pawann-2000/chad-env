9e17009dd0e9f054bbb1de24a739286ea8b21b2c
pawann-2000 <github.3jlwu@ggmaill.aleeas.com>
2024-09-22 22:17:05 +0530

    chore: Upload nvim config

---
+++ nvim/lua/pawan/plugins/lualine.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local lualine = require("lualine")
+    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+    local current_theme = require("lualine.themes.auto")
+
+    lualine.setup({
+      options = {
+        theme = current_theme,
+      },
+      sections = {
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" },
+          },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
+        },
+      },
+    })
+  end,
+}
